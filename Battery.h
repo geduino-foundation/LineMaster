783c7cf34906b2dcbf35b7d549ad85981d160a8b
Alessandro Francescon <alessandro.francescon@gmail.com>
2016-11-09 20:45:55 +0100

    added license header
    Conflicts:
    	SerialSetup.cpp
    	SerialSetup.h
    	Telemetry.cpp
    	Telemetry.h
    	pwm_frequency.h
    	types.h

--- Battery.h
+++ Battery.h
@@ -1,3 +1,20 @@
+/*
+  Battery.h
+  Copyright (C) 2016 Alessandro Francescon
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef _BATTERY_H_
 #define _BATTERY_H_
 
@@ -12,7 +29,7 @@ class Battery {
       // Initialization
       init();
     
-    };
+    }
 
     void readVoltage(float * volts);
 
